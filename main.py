c5a3869e0b605b9c0d517ca97f7a4b8c3ec0f6c5
Paky29 <p.somma11@studenti.unisa.it>
2023-06-12 16:30:59 +0200

    feature selection on the clenead SEERA dataset

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ from ensemble import LinearRegression_MetaModel, SVR_MetaModel, LinearRegression
 from models import ElasticNet, GradientBoostingRegression, KNearestNeighbors, RandomForestRegressor, \
     SupportVectorRegression
 
-seera = pd.read_csv("datasets/SEERA.csv", delimiter=',', decimal=".")
-X = seera.drop('Effort', axis=1)
-y = seera['Effort']
+def main():
+    seera = pd.read_csv("datasets/SEERA_cleaned.csv", delimiter=',', decimal=".")
+    X = seera.drop('Effort', axis=1)
+    y = seera['Effort']
 
-ElasticNet.run(X, y)
-GradientBoostingRegression.run(X, y)
-KNearestNeighbors.run(X, y)
-RandomForestRegressor.run(X, y)
-SupportVectorRegression.run(X, y)
+    #ElasticNet.run(X, y)
+    #GradientBoostingRegression.run(X, y)
+    #KNearestNeighbors.run(X, y)
+    #RandomForestRegressor.run(X, y)
+    #SupportVectorRegression.run(X, y)
 
-SVR_MetaModel.run(X, y)
+    #SVR_MetaModel.run(X, y)
 
-LinearRegression_MetaModel.run(X, y)
-LinearRegression_MetaModel2.run(X, y)
-LinearRegression_MetaModel3.run(X, y)
+    #LinearRegression_MetaModel.run(X, y)
+    #LinearRegression_MetaModel2.run(X, y)
+    LinearRegression_MetaModel3.runFeatureSelection(X, y)
+
+if __name__=="__main__":
+    main()
